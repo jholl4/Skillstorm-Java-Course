84a19ce00ecb972f50a711c0504ad1f562f178cd
Joshua Holland <joshua.holland.1@outlook.com>
2023-11-20 15:26:18 -0500

    Added some code to the test.java file to test an assignment answer I wasn't sure about

--- Test/src/com/skillstorm/test/Test.java
+++ Test/src/com/skillstorm/test/Test.java
@@ -6,7 +6,13 @@ public class Test {
 		boolean isHot = false;
 		boolean isHumid = false;
 		System.out.println(!(isHot||isHumid));
+		value((short) 2, (short)3);
 
+	}
+	
+	public static int value(short num1, short num2) {
+		int answer = num1*num2;
+		return answer;
 	}
 
 }
