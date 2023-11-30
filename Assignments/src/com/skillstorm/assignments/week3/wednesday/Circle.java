1e1f387404a7c26e4aa6ada087336e9d1aa028b9
Joshua Holland <joshua.holland.1@outlook.com>
2023-11-30 07:58:08 -0500

    added javadoc comments to circle methods

--- Assignments/src/com/skillstorm/assignments/week3/wednesday/Circle.java
+++ Assignments/src/com/skillstorm/assignments/week3/wednesday/Circle.java
@@ -11,19 +11,32 @@ public class Circle {
 	public Circle(double radius) {
 		this.radius = radius;
 	}
-	
+	/**
+	 * 
+	 * @return calculated area
+	 */
 	public double area() {
 		return Math.PI * Math.pow(this.radius, 2);
 	}
-	
+	/**
+	 * 
+	 * @return calculated diameter
+	 */
 	public double diameter(){
 		return Math.PI * 2 * this.radius;
 	}
-	
+	/**
+	 * 
+	 * @return calculated circumference
+	 */
 	public double circumference() {
 		return 2 * Math.PI * this.radius;
 	}
-	
+	/**
+	 * 
+	 * @param otherCircle
+	 * @return boolean; true if param circle equal to this circle
+	 */
 	public boolean isSameCircle(Circle otherCircle) {
 		if(otherCircle==null) {
 			return false;
