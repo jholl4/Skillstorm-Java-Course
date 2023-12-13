8a20d01371d8eca2317ae64380d1985f95f77f90
Joshua Holland <joshua.holland.1@outlook.com>
2023-12-13 17:16:17 -0500

    week5 wednesday groupwork and daily assignment

---
+++ IteratorPractice/src/com/skillstorm/Iterables.java
@@ -0,0 +1,16 @@
+package com.skillstorm;
+
+import java.util.ArrayList;
+
+public class Iterables {
+	
+	// Iterables have two methods
+	// under the hood let's use an iterator instead of for each!
+	
+	public static void main(String[] args) {
+		byte grades = (byte) 10101110;
+	}
+	
+	
+
+}
