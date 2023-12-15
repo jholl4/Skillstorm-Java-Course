2e0947f878b80f138456d0ca23f06f3bfb80eb5a
Joshua Holland <joshua.holland.1@outlook.com>
2023-12-15 16:02:37 -0500

    week 5 friday test review assignment

--- ArrayListPractice/ArrayPractice.java
+++ ArrayListPractice/ArrayPractice.java
@@ -3,13 +3,14 @@ import java.util.Arrays;
 public class ArrayPractice {
 
     public static void main(String[] args) {
-        int[] balances1 = new int[2];
-        balances1[0] = 10;
-        balances1[1] = 20;
+        // int[] balances1 = new int[2];
+        // balances1[0] = 10;
+        // balances1[1] = 20;
 
-        int[] balances2 = balances1;
-        balances2[0] = 0;
+        // int[] balances2 = balances1;
+        // balances2[0] = 0;
 
-        System.out.println(Arrays.toString(balances1));
+        // System.out.println(Arrays.toString(balances1));
+
     }
 }
